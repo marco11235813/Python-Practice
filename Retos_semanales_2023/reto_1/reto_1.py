fd287d33ee415aed2485dabb82d4e665ccce81ed
marquinho <marki_62@hotmail.com>
2023-10-16 07:36:18 -0300

    actualizacion

--- Retos_semanales_2023/reto_1/reto_1.py
+++ Retos_semanales_2023/reto_1/reto_1.py
@@ -1,12 +1,15 @@
-# 1-
-#  * Escribe un programa que muestre por consola (con un print) los
-#  * números de 1 a 100 (ambos incluidos y con un salto de línea entre
-#  * cada impresión), sustituyendo los siguientes:
-#  * - Múltiplos de 3 por la palabra "fizz".
-#  * - Múltiplos de 5 por la palabra "buzz".
-#  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+"""1-
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz"."""
 
-
+#----------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------
+"""Marco Caro PYandDT"""
 # Generamos los valores
 
 def elementos()-> list|tuple:
---
+++ Retos_semanales_2023/reto_3/reto_3.py
@@ -0,0 +1,133 @@
+ """* 3-Escribe un programa que muestre cómo transcurre un juego de tenis y quién lo ha ganado.
+ * El programa recibirá una secuencia formada por "P1" (Player 1) o "P2" (Player 2), según quien
+ * gane cada punto del juego.
+ * 
+ * - Las puntuaciones de un juego son "Love" (cero), 15, 30, 40, "Deuce" (empate), ventaja.
+ * - Ante la secuencia [P1, P1, P2, P2, P1, P2, P1, P1], el programa mostraría lo siguiente:
+ *   15 - Love
+ *   30 - Love
+ *   30 - 15
+ *   30 - 30
+ *   40 - 30
+ *   Deuce
+ *   Ventaja P1
+ *   Ha ganado el P1
+ * - Si quieres, puedes controlar errores en la entrada de datos.   
+ * - Consulta las reglas del juego si tienes dudas sobre el sistema de puntos. """
+
+#----------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------
+"""Marco Caro PYandDT"""
+
+
+def game()->str:
+    
+    game= input('Ingrese puntos del game: ').title().strip('[]').split(',')
+    game= [x.strip() for x in game]
+
+    for x in game:
+        if x not in ['P1', 'P2']:
+            print('Error en la entrada de datos')
+            return
+        else:
+            continue
+
+    valores= ('love','15','30','40', 'Ventaja ')
+    P1= 0
+    P2= 0
+    print('P1---P2\n')
+
+
+    for punto in game:
+        if P1 >= 4 and P2 >= 4:
+            pass
+        elif P1 == 0 and P2 == 0:
+            pass
+        else:
+            print(valores[P1]+' - '+valores[P2])
+
+
+        if P1 == 4 and P2 == 4:
+            print('Deuce')
+        elif punto == 'P1':
+            P1 += 1
+            if P1 >= 4 and (P1 - P2) == 2:
+                print(f'Ha ganado el {punto}')
+                return 
+            elif P1 == 5 and (P1 - P2) == 1:
+                print(valores[P1] + punto)
+                continue
+        elif punto == 'P2':
+            P2 += 1
+            if P2 >= 4 and (P2 - P1) == 2:
+                print(valores[P2] + punto)
+                return 
+            elif P2 == 5 and (P2 - P1) == 1:
+                print('Ventaja P2')
+                continue
+
+
+    return
+
+# otra manera
+
+def control_entrada_datos()->list|tuple:
+
+    lista= input('Ingrese puntos del game: ').title().strip('[]').split(',')
+    lista= [x.strip() for x in lista]
+
+    for x in lista:
+        if x not in ['P1', 'P2']:
+            print('Error en la entrada de datos')
+            return
+        else:
+            continue
+
+    return lista
+
+def game()->str:
+
+    try:   
+        game= control_entrada_datos()
+
+        valores= ('love','15','30','40', 'Ventaja ')
+        P1= 0
+        P2= 0
+        print('P1---P2\n')
+
+
+        for punto in game:
+            if P1 >= 4 and P2 >= 4:
+                pass
+            elif P1 == 0 and P2 == 0:
+                pass
+            else:
+                print(valores[P1]+' - '+valores[P2])
+
+            if P1 == 4 and P2 == 4:
+                print('Deuce')
+            elif punto == 'P1':
+                P1 += 1
+                if P1 >= 4 and (P1 - P2) == 2:
+                    print(f'Ha ganado el {punto}')
+                    return 
+                elif P1 == 5 and (P1 - P2) == 1:
+                    print(valores[P1] + punto)
+                    continue
+            elif punto == 'P2':
+                P2 += 1
+                if P2 >= 4 and (P2 - P1) == 2:
+                    print(valores[P2] + punto)
+                    return 
+                elif P2 == 5 and (P2 - P1) == 1:
+                    print('Ventaja P2')
+                    continue
+    except TypeError:
+        pass
+
+    return
+
+#----------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------
